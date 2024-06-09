3eaa87df0db97c61de2629b66763c050b5348d39
sathish-chanda <sathishchandas@gmail.com>
2024-06-09 01:08:52 -0400

    Basic SQLand Java project setup for postgres!

---
+++ Java-JDBC/src/main/java/com/satish/lil/jdbc/JDBCExecutor.java
@@ -0,0 +1,11 @@
+package com.satish.lil.jdbc;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class JDBCExecutor {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello Learning JDBC!");
+    }
+}
