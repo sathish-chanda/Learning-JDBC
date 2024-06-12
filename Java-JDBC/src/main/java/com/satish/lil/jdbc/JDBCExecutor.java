52c2a63523eaf02f59dca2f9cc1f8a57a0df6e24
sathish-chanda <sathishchandas@gmail.com>
2024-06-11 22:09:44 -0400

    Performing CRUD operations using DataAccessObject and DataTransferObject.

--- Java-JDBC/src/main/java/com/satish/lil/jdbc/JDBCExecutor.java
+++ Java-JDBC/src/main/java/com/satish/lil/jdbc/JDBCExecutor.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,6 +13,7 @@ public class JDBCExecutor {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost","hplussport","postgres","password");
+        CustomerDAO customerDAO;
         try{
             Connection connection = dcm.getConnection();
             Statement statement = connection.createStatement();
@@ -19,6 +21,37 @@ public class JDBCExecutor {
             while(resultSet.next()) {
                 System.out.println(resultSet.getInt(1));
             }
+
+            // Insert
+            customerDAO = new CustomerDAO(connection);
+            Customer dummy = new Customer();
+            dummy.setFirstName("Michel");
+            dummy.setLastName("Jackson");
+            dummy.setAddress("520 Saint-Marc");
+            dummy.setCity("Toronto");
+            dummy.setEmail("michel.jackson@gmail.com");
+            dummy.setPhone("9876 543 210");
+            dummy.setState("Ontario");
+            dummy.setZipCode("H4F 1W3");
+            Customer added = customerDAO.create(dummy);
+
+            // findById
+            customerDAO = new CustomerDAO(connection);
+            Customer customer = customerDAO.findById(added.getId());
+            System.out.println(customer);
+
+            // Update
+            customer.setPhone("(987)654-3210");
+            customerDAO.update(customer);
+            customer = customerDAO.findById(added.getId());
+            System.out.println(customer);
+
+            // Delete
+            customerDAO.delete(customer.getId());
+
+            List<Customer> customers = customerDAO.findAll();
+            System.out.println("List size = " + customers.size());
+
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
         }
---
+++ Java-JDBC/src/main/java/com/satish/lil/jdbc/util/DataTransferObject.java
@@ -0,0 +1,6 @@
+package com.satish.lil.jdbc.util;
+
+public interface DataTransferObject {
+
+    long getId();
+}
